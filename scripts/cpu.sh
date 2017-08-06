ae2681b768e64411736c2940fedc15275f81acac
Piotr Wittchen <piotr@wittchen.biz.pl>
2017-08-06 15:10:11 +0200

    adding project files

---
+++ scripts/cpu.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "$CURRENT_DIR/helpers.sh"
+
+print_cpu_usage() {
+  if is_linux; then
+    mpstat | awk '$12 ~ /[0-9.]+/ { print 100 - $12"%" }'
+  elif is_osx; then
+    ps -A -o %cpu | awk '{s+=$1} END {print s "%"}'
+  fi
+}
+
+main() {
+  print_cpu_usage
+}
+
+main
